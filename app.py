7ad516b943b6c4edaa740d84fa2ef829933a824d
siriusCodes <112203737+siriusCodes@users.noreply.github.com>
2022-08-26 15:53:54 -0400

    Update app.py
    Added a dataframe scraped from a website

--- app.py
+++ app.py
@@ -3,3 +3,14 @@ import pandas as pd
 
 st.title("Demo Dashboard")
 st.write("Example dashboard in Marquee Class")
+
+categories = ['A','B','C']
+st.multiselect('pick an option', categories)
+
+st.sidebar.button('Click me!')
+
+url = "https://www.iposcoop.com/last-100-ipos/"
+dfs = pd.read_html(url)
+df = dfs[0]
+
+
